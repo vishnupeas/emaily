709533936366f5f556333b647f21b6280ce0a89c
woomiz <vishnups1998@gmail.com>
2019-12-16 22:17:45 +0530

    getting done with nodemon setup and passport fetching user info

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+console.log(keys);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access Token\n" + accessToken);
+      console.log("refresh Token\n" + refreshToken);
+      console.log("profile");
+      console.log(profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
