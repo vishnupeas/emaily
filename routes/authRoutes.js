610d9157faa72807e80a3990e5fa7b35e0b307cb
woomiz <vishnups1998@gmail.com>
2019-12-17 11:57:28 +0530

    getting done with the small refactor

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,18 @@
 const passport = require("passport");
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google"),
-  (req, res) => {
-    res.redirect("/");
-  }
-);
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
+};
