f4a332595b58e7e00df986908665e2bc23168c99
woomiz <vishnups1998@gmail.com>
2019-12-17 14:40:50 +0530

    making the mongodb work with mongoose and connect created model to the database we need

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const keys = require("./config/keys");
-require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+require("./models/User");
+
+require("./services/passport");
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,7 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access Token\n" + accessToken);
-      console.log("refresh Token\n" + refreshToken);
-      console.log("profile");
-      console.log(profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
