00eb63efd5a559ce80d8a124370c9cc9652fd8a4
woomiz <vishnups1998@gmail.com>
2019-12-21 18:37:55 +0530

    solved issues around the concurrently and now we are ready to go

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,6 @@ module.exports = app => {
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
   });
   app.get("/api/current_user", (req, res) => {
     console.log(req.user);
