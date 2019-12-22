6ef20f156b9533fb096ad71d4d6a89ac14d762c7
woomiz <vishnups1998@gmail.com>
2019-12-22 18:02:19 +0530

    CREATING THE ERROR 404 router and components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Error404 from "./Error404";
 import Header from "./Header";
 const Dashboard = () => <h2> Dashboard </h2>;
 const SurveyNew = () => <h2> SurvayNew </h2>;
@@ -12,10 +13,10 @@ function App() {
       <div>
         <BrowserRouter>
           <div>
-            <a href="/auth/google/">Sign In with google</a>
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
             <Route path="/surveys/new" component={SurveyNew} />
+            <Route path="/" component={Error404} />
           </div>
         </BrowserRouter>
       </div>
