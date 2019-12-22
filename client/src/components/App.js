937a82ea2d6815b298130bae4d6727afed256846
woomiz <vishnups1998@gmail.com>
2019-12-22 17:13:50 +0530

    making use the webpack to import the materialize-css and make use of it.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+const Dashboard = () => <h2> Dashboard </h2>;
+const SurveyNew = () => <h2> SurvayNew </h2>;
+const Landing = () => <h2> Landing </h2>;
 
 function App() {
   return (
     <React.Fragment>
-      <a href="/auth/google/">Sign In with google</a>
+      <div>
+        <BrowserRouter>
+          <div>
+            <a href="/auth/google/">Sign In with google</a>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     </React.Fragment>
   );
 }
