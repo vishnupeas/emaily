ed87bd75b6df7880d3e9ad4ee23d03bc012e86c8
woomiz <vishnups1998@gmail.com>
2019-12-30 23:15:31 +0530

    getting done with the creation of new variable to the user model and setting up of charges

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleId: String,
   googleName: String,
-  googleEmail: String
+  googleEmail: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model("users", userSchema);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,17 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {});
+  app.post("/api/stripe", async (req, res) => {
+    // console.log(req.body);
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "$5 for 5 credits",
+      source: req.body.id
+    });
+    // console.log(charge);
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  });
 };
