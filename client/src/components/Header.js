3baefcd241f6356747050c0c766615480cf9743f
woomiz <vishnups1998@gmail.com>
2020-01-05 02:35:13 +0530

    making use of the materialize css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "materialize-css/dist/css/materialize.min.css";
 import Payments from "./Payments";
 
 class Header extends Component {
