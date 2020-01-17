046d40a881ffc9c50d2a761224b9804de92ff0fc
woomiz <vishnups1998@gmail.com>
2020-01-18 00:36:28 +0530

    understanding the flow of the working...

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,7 +11,12 @@ module.exports = app => {
     const survey = new Survey({
       title,
       subject,
-      body
+      body,
+      recipients: recipients.split(",").map(email => {
+        email
+      }),
+      _user:req.user.id,
+      dateSent: Date.now()
     });
   });
 };
