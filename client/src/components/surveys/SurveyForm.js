fa949ee52be82e5ca4c7da75afcde44278ed7df6
woomiz <vishnups1998@gmail.com>
2020-02-01 19:39:22 +0530

    getting a SurveyForm.js started and setting up redux-dev tools

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,21 @@
+// Getting a form displayed for user input
+
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+
+class SurveyForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="survyTitle" component="input" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,14 @@
+//getting a global component to manage both the SurveyNew and SurveyReview
 import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
 
 class SurveyNew extends Component {
   render() {
-    return <div>SurveyNew!</div>;
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
   }
 }
 
