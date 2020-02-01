3126e84f2a9d7f22a3b1336a196f9a39c13d384d
woomiz <vishnups1998@gmail.com>
2020-02-01 11:21:19 +0530

    adding materialize icon on bottom

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-lare red">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
