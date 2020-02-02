e7da91006bf0bb87fc95953f90bd6dc05865b873
woomiz <vishnups1998@gmail.com>
2020-02-02 12:08:49 +0530

    getting started with the form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,26 @@
 //getting a global component to manage both the SurveyNew and SurveyReview
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
-  render() {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
     return (
-      <div>
-        <SurveyForm />
-      </div>
+      <SurveyForm
+        onSurveySubmit={values => {
+          this.setState({ showFormReview: true });
+        }}
+      />
     );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
