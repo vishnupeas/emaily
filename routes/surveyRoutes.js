a320dcb39565c4f6958ab10eb5b793992f4749fe
woomiz <vishnups1998@gmail.com>
2020-02-06 00:45:19 +0530

    making the localtunnel to work

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,28 +1,37 @@
-const requireLogin = require("../middlewares/requireLogin");
-const requireCredits = require("../middlewares/requireCredits");
-const mongoose = require("mongoose");
-const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const mongoose = require('mongoose');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
-const Survey = mongoose.model("surveys");
+const Survey = mongoose.model('surveys');
 
-module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
-    const { title, subject, body, recipients } = req.body;
+module.exports = (app) => {
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
 
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients: recipients.split(",").map(email => ({
-        email:email.trim()
-      })),
-      _user: req.user.id,
-      dateSent: Date.now()
-    });
+		app.post('/api/surveys/webhooks', (req, res) => {
+			console.log(req.body);
+			res.send({});
+		});
 
-     // Great place to send an email!
-    const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
-  });
+		app.get('/api/surveys/nice', (req, res) => {
+			res.send({"send":"nice"});
+		});
+
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map((email) => ({
+				email: email.trim()
+			})),
+			_user: req.user.id,
+			dateSent: Date.now()
+		});
+
+		// Great place to send an email!
+		const mailer = new Mailer(survey, surveyTemplate(survey));
+		mailer.send();
+	});
 };
