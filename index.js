5bb35ecc74e14889ca938cb4108900c7a34534d1
woomiz <vishnups1998@gmail.com>
2020-02-05 16:21:26 +0530

    done testing on wsl

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
-//teting out vs code
-
 //mongoose connecting to the mongodb database that we define and making use of the User model class
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
