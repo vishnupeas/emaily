98ee894a5a5a72e91029de4b6a4eb1d2c0b5381c
woomiz <vishnups1998@gmail.com>
2020-02-06 21:45:23 +0530

    added  path-parser,url and lodash

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,37 +1,44 @@
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-const mongoose = require('mongoose');
-const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 
-const Survey = mongoose.model('surveys');
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const mongoose = require("mongoose");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
-module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-		const { title, subject, body, recipients } = req.body;
+const Survey = mongoose.model("surveys");
 
-		app.post('/api/surveys/webhooks', (req, res) => {
-			console.log(req.body);
-			res.send({});
-		});
+module.exports = app => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
-		app.get('/api/surveys/nice', (req, res) => {
-			res.send({"send":"nice"});
-		});
+    app.post("/api/surveys/webhooks", (req, res) => {
+      const events = _.map(req.body, event => {
+        const pathname = new URL(event.url).pathname;
+        const p = new Path("api/surveys/:surveyId/:choice");
+        console.log(p.test(pathname));
+      });
+    });
 
-		const survey = new Survey({
-			title,
-			subject,
-			body,
-			recipients: recipients.split(',').map((email) => ({
-				email: email.trim()
-			})),
-			_user: req.user.id,
-			dateSent: Date.now()
-		});
+    app.get("/api/surveys/nice", (req, res) => {
+      res.send({ send: "nice" });
+    });
 
-		// Great place to send an email!
-		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
-	});
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({
+        email: email.trim()
+      })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
+  });
 };
