06b6639f9ed5e3020f7a0d0315c921b6c90da833
woomiz <vishnups1998@gmail.com>
2020-02-08 10:34:52 +0530

    connecting the components to fetch the surveys

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSurveys } from "../../actions";
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+  render() {
+    return <div>SurveyLlist</div>;
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
